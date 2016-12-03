e7b5ec4f842222f020af1e79108f1fc6e5c3f093
Fanil.Kashapov <fanil.kashapov@eleks.com>
2016-12-03 18:13:34 +0200

    add geo api

---
+++ client/src/app/directives/geo.js
@@ -0,0 +1,34 @@
+'use strict';
+
+class GeolocationController {
+    constructor ($scope, $state, $window, $timeout) {
+        this._$scope = $scope; 
+        this._$state = $state;
+        this._$window = $window;
+        this._$timeout = $timeout;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+        self._$timeout(()=> {
+            navigator.geolocation.getCurrentPosition((position) => {
+                self.speed = position.coords.speed;
+                self.init();
+            });
+        }, 400);
+    }
+}
+
+GeolocationController.$inject = ['$scope', '$state', '$window', '$timeout'];
+
+export default class Geo {
+    constructor() {
+        this.restrict = 'AE';
+        this.replace = true;
+        this.controller = GeolocationController;
+        this.controllerAs = 'geoLoc';
+        this.bindToController = true;
+        this.template = '<span>{{geoLoc.speed}}</span>';
+    }
+}
