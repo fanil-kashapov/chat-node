7fccecfa4209024e812ff0d00e151456a554a164
Fanil.Kashapov <fanil.kashapov@eleks.com>
2016-12-09 18:47:23 +0200

    git midify karma test runner config

--- karma.conf.js
+++ karma.conf.js
@@ -2,8 +2,12 @@ module.exports = function(config) {
     config.set({
 
         basePath: '.',
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['browserify', 'jasmine'],
 
+        // list of files / patterns to load in the browser
         files: [
             'node_modules/angular/angular.min.js',
             'node_modules/angular-mocks/angular-mocks.js',
@@ -16,29 +20,71 @@ module.exports = function(config) {
         exclude: [
         ],
 
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
             'src/app.js': ['browserify'],
             'src/**/*.js': ['browserify'],
             'test/**/*test.js': ['browserify']
         },
 
+        // configure jasmine.frameworks
         browserify: {
             debug: true,
-            transform: ['babelify', 'stringify']
+            transform: [['babelify', { presets: ['es2015'] /*, plugins: ['transform-object-assign', 'array-includes', 'es6-promise'] */}]]
         },
 
-        reporters: ['progress'],
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['mocha'],
 
+         // List of plugins to load.
+        // A plugin can be a string (in which case it will be required by Karma)
+        // or an inlined plugin - Object.
+        // By default, Karma loads all sibling NPM modules which have a name starting
+        // with karma-*.
+        //plugins: ['karma-browserify', 'karma-jasmine', 'karma-phantomjs-launcher', 'karma-spec-reporter'],
+
+        // web server port
         port: 9876,
 
+        // enable / disable colors in the output (reporters and logs)
         colors: true,
 
-        logLevel: config.LOG_DEBUG,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
+        // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
 
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['PhantomJS'],
 
-        singleRun: false
-    });
-};
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
+    })
+}
+
+    
+
+
+    
+
+
+    
+
+
+    
+
+
+    
+
+    
