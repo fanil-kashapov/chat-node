0a280578aaf6b2f9570d6f61b0bcc64e9d59b0ed
Fanil.Kashapov <fanil.kashapov@eleks.com>
2016-12-13 19:07:47 +0200

    fix bug time service

--- src/app/services/time.js
+++ src/app/services/time.js
@@ -9,7 +9,7 @@ export default class TimeAgo {
             $minutes = Math.round(time_elapsed / 60),
             $hours = Math.round(time_elapsed / 3600),
             $days = Math.round(time_elapsed / 86400),
-            $weeks = Math.round(time_elapsed / 604800),
+            $weeks = Math.round(time_elapsed / 604800 * 10) / 10,
             $months = Math.round(time_elapsed / 2600640),
             $years = Math.round(time_elapsed / 31207680);
 
@@ -20,7 +20,7 @@ export default class TimeAgo {
             return 'least one minute';
         }
         //$Minutes
-        else if ($minutes <= 60) {
+        else if ($minutes < 60) {
             if ($minutes == 1) {
                 return 'one minute ago';
             } else {
@@ -28,7 +28,7 @@ export default class TimeAgo {
             }
         }
         //$Hours
-        else if ($hours <= 24) {
+        else if ($hours < 24) {
             if ($hours == 1) {
                 return 'an hour ago';
             } else {
@@ -36,27 +36,27 @@ export default class TimeAgo {
             }
         }
         //$Days
-        else if ($days <= 7) {
+        else if ($days < 7) {
             if ($days == 1) {
                 return 'yesterday';
             } else {
-                return $days + ' $days ago';
+                return $days + ' days ago';
             }
         }
         //$Weeks
-        else if ($weeks <= 4.3) {
+        else if ($weeks < 4.2) {
             if ($weeks == 1) {
                 return 'a week ago';
             } else {
-                return $weeks + ' $weeks ago';
+                return $weeks + ' weeks ago';
             }
         }
         //$Months
-        else if ($months <= 12) {
+        else if ($months < 12) {
             if ($months == 1) {
                 return 'a month ago';
             } else {
-                return $months + ' $months ago';
+                return $months + ' months ago';
             }
         }
         //$Years
@@ -64,7 +64,7 @@ export default class TimeAgo {
             if ($years == 1) {
                 return 'one year ago';
             } else {
-                return $years + ' $years ago';
+                return $years + ' years ago';
             }
         }
     }
