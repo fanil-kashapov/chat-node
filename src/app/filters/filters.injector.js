54595f472e4f03233e099250e927ed35258c77d6
Andrii <andriy.vandakurov@gmail.com>
2017-01-20 00:11:06 +0200

    Code comments

--- src/app/filters/filters.injector.js
+++ src/app/filters/filters.injector.js
@@ -2,6 +2,11 @@
 
 import filters from './filters';
 
+// TODO: Please write it in more compact way e.g.
+// const ngModule = angular.module('chatApp.filters', [])
+// ngModule.factory('Filters', filters.FiltersFactory)
+// export default moduleName.name;
+
 var moduleName = 'chatApp.filters';
 angular.module(moduleName, []);
 
--- src/app/services/constants.js
+++ src/app/services/constants.js
@@ -2,6 +2,8 @@
 
 class CONSTANT {
     constructor() {
+        // TODO: Use thia as angular .const instead
+        // You don't need es2015 class for this
         this.FACEBOOK = {
             APP_ID: '1765321937087529',
             APP_KEY: 'c1f8847b1a6db54600119fd8fbe628fb'
