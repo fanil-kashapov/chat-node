8fec74043f20f4717448ebad0dd02da007341897
Fanil Kashapov <Fanil Kashapov>
2017-01-22 14:14:41 +0200

    refactore time service

--- src/app/services/time.js
+++ src/app/services/time.js
@@ -1,75 +1,62 @@
 export default class TimeAgo {
-    constructor() {}
+    timeAgo (date) {
+        const messageTime = new Date(date),
+            curTime = new Date(),
+            timeElapsed = (curTime - messageTime) / 1000,
+            $seconds = timeElapsed,
+            $minutes = Math.round(timeElapsed / 60),
+            $hours = Math.round(timeElapsed / 3600),
+            $days = Math.round(timeElapsed / 86400),
+            $weeks = Math.round(timeElapsed / 604800 * 10) / 10,
+            $months = Math.round(timeElapsed / 2600640),
+            $years = Math.round(timeElapsed / 31207680);
 
-    timeAgo(date) {
-        let messageTime = new Date(date),
-            cur_time = new Date(),
-            time_elapsed = (cur_time - messageTime) / 1000,
-            $seconds = time_elapsed,
-            $minutes = Math.round(time_elapsed / 60),
-            $hours = Math.round(time_elapsed / 3600),
-            $days = Math.round(time_elapsed / 86400),
-            $weeks = Math.round(time_elapsed / 604800 * 10) / 10,
-            $months = Math.round(time_elapsed / 2600640),
-            $years = Math.round(time_elapsed / 31207680);
+        switch (true) {
+            //  $Seconds
+            case ($seconds <= 10):
+                return 'just now';
+            case ($minutes === 0):
+                return 'least one minute';
 
-        // $Seconds
-        if ($seconds <= 10) {
-            return 'just now';
-        } else if ($minutes == 0) {
-            return 'least one minute';
-        }
-        //$Minutes
-        else if ($minutes < 60) {
-            if ($minutes == 1) {
-                return 'one minute ago';
-            } else {
-                return $minutes + ' minutes ago';
-            }
-        }
-        //$Hours
-        else if ($hours < 24) {
-            if ($hours == 1) {
-                return 'an hour ago';
-            } else {
-                return $hours + ' hrs ago';
-            }
-        }
-        //$Days
-        else if ($days < 7) {
-            if ($days == 1) {
-                return 'yesterday';
-            } else {
-                return $days + ' days ago';
-            }
-        }
-        //$Weeks
-        else if ($weeks < 4.2) {
-            if ($weeks == 1) {
-                return 'a week ago';
-            } else {
-                return $weeks + ' weeks ago';
-            }
-        }
-        //$Months
-        else if ($months < 12) {
-            if ($months == 1) {
-                return 'a month ago';
-            } else {
-                return $months + ' months ago';
-            }
-        }
-        //$Years
-        else {
-            if ($years == 1) {
+            //  $Minutes
+            case ($minutes < 60):
+                if ($minutes === 1)
+                    return 'one minute ago';
+                return `${$minutes} minutes ago`;
+
+            //  $Hours
+            case ($hours < 24):
+                if ($hours === 1)
+                    return 'an hour ago';
+                return `${$hours} hrs ago`;
+
+            //  $Days
+            case ($days < 7):
+                if ($days === 1)
+                    return 'yesterday';
+                return `${$days} +  days ago`;
+
+            //  $Weeks
+            case ($weeks < 4.2):
+                if ($weeks === 1)
+                    return 'a week ago';
+                return `${$weeks} weeks ago`;
+
+            //$Months
+            case ($months < 12):
+                if ($months === 1)
+                    return 'a month ago';
+                return `${$months} months ago`;
+
+            //$Years
+            case ($years === 1):
                 return 'one year ago';
-            } else {
-                return $years + ' years ago';
-            }
+            default:
+                return `${$years} years ago`;
         }
     }
 
-    static TimeAgoFactory() {
+    static TimeAgoFactory () {
         return new TimeAgo();
     }
 }
